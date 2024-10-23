c7bc29d643718959f7170bb88d926b6ebfd7bb86
PopshaNazar <nazarpopsha@gmail.com>
2024-10-23 22:22:38 +0300

    +trucks(1)

--- trucks/Main.java
+++ trucks/Main.java
@@ -1,12 +1,51 @@
 package trucks;
 
+class Truck {
+    private static int maxTows = 5;
+    private Tow[] tows = new Tow[maxTows];
+    private int towsNr = 0;
+
+    public boolean addTow(int loadNr, String regNr) {
+        if (towsNr < maxTows) {
+            tows[towsNr] = new Tow(loadNr, regNr);
+            towsNr++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addTow(String regNr) {
+        if (towsNr < maxTows) {
+            tows[towsNr] = Tow;
+
+            towsNr++;
+            return true;
+        }
+        return false;
+    }
+}
+
 class Tow {
-    public int loadNr;
-    public String regNr;
+    private int loadNr;
+    private String regNr;
+
+    private static int lastLoadNr = 0;
 
     public void addTow(int loadNr, String regNr) {
         this.loadNr = loadNr;
         this.regNr = regNr;
+        lastLoadNr = loadNr;
+    }
+
+    public void addTow(String regNr) {
+        this.regNr = regNr;
+        if (lastLoadNr == 0) {
+            this.loadNr = 10;
+        } else {
+            this.loadNr = lastLoadNr + 1;
+        }
+        lastLoadNr = this.loadNr;
+
     }
 
     public String getTow() {
@@ -16,10 +55,12 @@ class Tow {
 }
 
 public class Main {
-    public static void main(String argv[]) {
-        Tow tow1 = new Tow();
-        tow1.addTow(12, "TM777S");
-        System.out.println(tow1.getTow());
-
+    public static void main(String args[]) {
+        Truck[] tow = new Truck[5];
+        //Tow tow1 = new Tow();
+        //tow1.addTow(12, "TM777S");
+        //System.out.println(tow1.getTow());
+        for (int i = 0; i < tow.length; i++) {
+            
     }
 }
